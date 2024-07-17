6b7fefb99a1b91a94a50b22cd3422bc9ed508d70
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-17 18:43:27 +0800

    finished the ui

--- lib/arithmetic_button.dart
+++ lib/arithmetic_button.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ArithmeticButton extends StatelessWidget {
-  const ArithmeticButton(this.text, {super.key, required this.operation});
+  const ArithmeticButton(this.text, {super.key, this.operation});
 
   final String text;
-  final void Function() operation;
+  final void Function()? operation;
   @override
   Widget build(context) {
     return ElevatedButton(
-      onPressed: operation,
-      child: Text(text),
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: const Color.fromARGB(255, 67, 62, 62),
+      ),
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:calculator_app/arithmetic_button.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -17,15 +18,55 @@ class _MainScreenState extends State<MainScreen> {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
+                Color.fromARGB(255, 255, 255, 255),
+                Color.fromARGB(255, 133, 128, 128),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
           ),
-          child: const Center(
-            child: Text("Result"),
+          child: Center(
+            child: SizedBox(
+              width: double.infinity,
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Input first value:',
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Input second value:',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    const ArithmeticButton("Addition"),
+                    const ArithmeticButton("Subtraction"),
+                    const ArithmeticButton("Multiplication"),
+                    const ArithmeticButton("Division"),
+                    const SizedBox(height: 80),
+                    const Text(
+                      "Result",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
